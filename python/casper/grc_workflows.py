10532736c2e12a2bcc0fcbb21d383828057970cd
Wei Liu <liuwei_berkeley@berkeley.edu>
2025-05-21 10:47:00 -0700

    generated jasper.json.

--- python/casper/grc_workflows.py
+++ python/casper/grc_workflows.py
@@ -11,6 +11,7 @@ import logging
 import os
 import subprocess
 import tempfile
+import json
 from pathlib import Path
 
 from gnuradio.grc.core import Messages
@@ -19,10 +20,12 @@ from gnuradio.grc.workflows import GeneratorBase
 from gnuradio.grc.workflows.common import add_xterm_to_run_command
 
 class FlowgraphInfo(object):
-    unnecessary_keys = ['alias', 'affinity', 'minoutbuf', 'maxoutbuf']
+    unnecessary_keys = ['alias', 'affinity', 'minoutbuf', 'maxoutbuf', 'comment']
     def __init__(self, flowgraph, input_file):
-        self.flowgraph = flowgraph
+        self.flow_graph = flowgraph
         self.input_file = input_file
+        self.builddir = self.input_file.split('.')[0]
+        os.makedirs(self.builddir, exist_ok=True)
         self.flowgraph_name = input_file.split('/')[-1].split('.')[0]
         self.flowgraph_info = {}
         self.flowgraph_info['project'] = {}
@@ -30,7 +33,17 @@ class FlowgraphInfo(object):
         self.flowgraph_info['project']['filename'] = self.input_file
         self.flowgraph_info['xps_blocks'] = []
         self.flowgraph_info['dsp_blocks'] = []
+        self.flowgraph_info['link_info'] = []
 
+    def _search_block_tag_by_name(self, name):
+        for b in self.flowgraph_info['xps_blocks']:
+            if b['name'] == name:
+                return 'xps'
+        for b in self.flowgraph_info['dsp_blocks']:
+            if b['name'] == name:
+                return 'dsp'
+        return None
+    
     def _get_block_tag(self, b):
         for param_id, param in b.params.items():
             if param_id == 'tag':
@@ -54,24 +67,47 @@ class FlowgraphInfo(object):
                 elif param_id == 'id':
                     block_info['name'] = str(param.value)
                 elif param_id == 'tag':
-                    block_info['tag'] = '%s:%s'%(tag, str(param.value))
+                    block_info['tag'] = '%s:%s'%(tag, b.key)
                 else:
                     block_info[param_id] = str(param.value)
                 block_info['fullpath'] = '%s/%s'%(self.flowgraph_name, block_info['name'] )
             if tag == 'xps':
-                self.flowgraph_info['xps_block'].append(block_info)
+                self.flowgraph_info['xps_blocks'].append(block_info)
             elif tag == 'dsp':
-                self.flowgraph_info['dsp_block'].append(block_info)
+                self.flowgraph_info['dsp_blocks'].append(block_info)
 
 
     def get_block_connections(self):
-        pass
-
-    def search_block_tag(self):
-        pass
+        for c in self.flow_graph.get_enabled_connections():
+            src_blk_name = c.source_block.name
+            src_blk_tag = self._search_block_tag_by_name(src_blk_name)
+            src_port_name = '%s_%s_%s'%(self.flowgraph_name ,src_blk_name, c.source_port.name)
+            src_port_id = int(c.source_port.key)
+            # TODO: get the port width from the port
+            src_port_width = 32
+            dst_blk_name = c.sink_block.name
+            dst_blk_tag = self._search_block_tag_by_name(dst_blk_name)
+            dst_port_name = '%s_%s_%s'%(self.flowgraph_name ,dst_blk_name, c.sink_port.name)
+            dst_port_id = int(c.sink_port.key)
+            # TODO: get the port width from the port
+            dst_port_width = 32
+            link_info = {}
+            link_info['src_blk_name'] = src_blk_name
+            link_info['src_port_name'] = src_port_name
+            link_info['src_port_width'] = src_port_width
+            link_info['src_port_id'] = src_port_id + 1
+            link_info['dst_blk_name'] = dst_blk_name
+            link_info['dst_port_name'] = dst_port_name
+            link_info['dst_port_width'] = dst_port_width
+            link_info['dst_port_id'] = dst_port_id + 1
+            link_info['link_type'] = '%s_%s'%(src_blk_tag, dst_blk_tag)
+            self.flowgraph_info['link_info'].append(link_info)
     
     def gen_jasper_json(self):
-        pass
+        filename = '%s/jasper.json'%self.builddir
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(self.flowgraph_info, f, ensure_ascii=False, indent=4)
+
 
 def _get_output_path(fg, output_dir):
     """Generate the output path for the image core artefacts.
@@ -91,44 +127,6 @@ def _get_output_path(fg, output_dir):
         return os.path.join(tempfile.gettempdir(), build_dir_name)
     return os.path.join(output_dir, build_dir_name)
 
-
-# def generate_build_command(input_file, build_dir, flow_graph):
-#     """Generate the build command for the RFNoC image."""
-#     args = ['rfnoc_image_builder', ]
-#     if flow_graph.get_option('fpga_dir'):
-#         args.extend(['--fpga-dir', flow_graph.get_option('fpga_dir')])
-#     build_dir = os.path.normpath(os.path.abspath(build_dir))
-#     build_output_dir = flow_graph.get_option('build_output') \
-#         if flow_graph.get_option('build_output') \
-#         else str(Path(input_file).parent)
-#     build_ip_dir = flow_graph.get_option('build_ip') \
-#         if flow_graph.get_option('build_ip') \
-#         else os.path.join(build_dir, 'build-ip')
-#     include_dirs = [d.strip() for d in flow_graph.get_option('include_dirs').split(os.pathsep) if d.strip()]
-#     for include_dir in include_dirs:
-#         args.extend(['-I', include_dir])
-#     args.extend([
-#         "--build-dir", build_dir,
-#         "--build-output-dir", build_output_dir,
-#         "--build-ip-dir", build_ip_dir])
-#     if flow_graph.get_option('jobs'):
-#         args.extend(['--jobs', str(flow_graph.get_option('jobs'))])
-#     else:
-#         args.extend(['--jobs', str(os.cpu_count)])
-#     if not flow_graph.get_option('include_hash'):
-#         args.append('--no-hash')
-#     if not flow_graph.get_option('include_date'):
-#         args.append('--no-date')
-#     if flow_graph.get_option('vivado_path'):
-#         args.extend(['--vivado-path', flow_graph.get_option('vivado_path')])
-#     if flow_graph.get_option('ignore_warnings'):
-#         args.append('--ignore-warnings')
-#     if flow_graph.get_option('reuse'):
-#         args.append('--reuse')
-#     args.extend(['--grc-config', input_file])
-#     return args
-
-
 class CASPERImageGenerator(GeneratorBase):
     """Generator class for RFNoC bitfiles from GRC."""
 
@@ -146,6 +144,7 @@ class CASPERImageGenerator(GeneratorBase):
         self.input_file = flow_graph.grc_file_path
         self.log = logging.getLogger(self.__class__.__name__)
         self.xterm = self.flow_graph.parent_platform.config.xterm_executable
+        self.fginfo = FlowgraphInfo(self.flow_graph, self.input_file)
         print('file_path', self.file_path)
         print('input_file', self.input_file)
 
@@ -169,19 +168,23 @@ class CASPERImageGenerator(GeneratorBase):
             Messages.send_verbose_exec(proc_info.stdout)
         """
         print('current path:', os.getcwd())
-        for b in self.flow_graph.get_enabled_blocks():
-            print(b.key)
-            for param_id, param in b.params.items():
-                print('param_id:', param_id)
-                print('param value:', param.value)
-            print(type(b))
-        for c in self.flow_graph.get_enabled_connections():
-            print(c)
-            print(c.source_block)
-            print(c.source_port)
-            print(c.sink_block)
-            print(c.sink_port)
-        image_builder_cmd = "cd /data/wei; ls"
+        # for b in self.flow_graph.get_enabled_blocks():
+        #     print(b.key)
+        #     for param_id, param in b.params.items():
+        #         print('param_id:', param_id)
+        #         print('param value:', param.value)
+        #     print(type(b))
+        # for c in self.flow_graph.get_enabled_connections():
+        #     print(c)
+        #     print('src_blk:', c.source_block)
+        #     print('src_blk.key:', c.source_block.key)
+        #     print('src_blk.name:', c.source_block.name)
+        #     print('src_port:', c.source_port)
+        #     print('src_port.key:', c.source_port.key)
+        #     print('src_port.name:', c.source_port.name)
+        #     print(c.sink_block)
+        #     print(c.sink_port)
+        # image_builder_cmd = "cd /data/wei; ls"
         # proc_info = subprocess.run(
         #     image_builder_cmd,
         #     stdout=subprocess.PIPE,
@@ -189,9 +192,21 @@ class CASPERImageGenerator(GeneratorBase):
         #     universal_newlines=True,
         #     shell=True
         # )
-        os.system(image_builder_cmd)
-        print('hello')
-        print('in write')
+        # os.system(image_builder_cmd)
+        self.fginfo.get_block_parameters()
+        self.fginfo.get_block_connections()
+        self.fginfo.gen_jasper_json()
+        scilab_library_path = os.getenv('MLIB_DEVEL_PATH')+'/scilab_library';
+        modelpath = self.input_file
+        frontend_cmd = scilab_library_path+'/jasper_frontend.py' + ' ' + '-m ' + modelpath
+        frontend_cmd = '/home/wei/miniconda3/envs/m2021a/bin/python %s'%(frontend_cmd)
+        print('frontend_cmd: ', frontend_cmd)
+        os.system(frontend_cmd)
+        cmd = {}
+        cmd['dsp'] = '/home/wei/miniconda3/envs/m2021a/bin/python /home/wei/scilab_demo/mlib_devel/scilab_library/gen_dsp_ip.py -m %s'%self.input_file
+        cmd['full'] = '/home/wei/miniconda3/envs/m2021a/bin/python /home/wei/scilab_demo/mlib_devel/jasper_library/exec_flow.py -m %s --middleware --backend --software --vitis'%self.input_file
+        os.system(cmd['dsp'])
+        os.system(cmd['full'])
 
 
     def get_exec_args(self):
